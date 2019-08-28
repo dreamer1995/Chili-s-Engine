b2d53aaa3306833d38d0106ed96c9be4f2d170cb
dreamer1995 <dreamer199506@126.com>
2019-08-29 01:11:22 +0800

    Fixed partial issues, but still gimbal lock

--- hw3d/Camera.cpp
+++ hw3d/Camera.cpp
@@ -23,7 +23,16 @@ DirectX::XMMATRIX Camera::GetMatrix() const noexcept
 	// camera "top" always faces towards +Y (cannot do a barrel roll)
 	const auto camPosition = XMLoadFloat3( &pos );
 	const auto camTarget = camPosition + lookVector;
-	return XMMatrixLookAtLH( camPosition,camTarget,XMVectorSet( 0.0f,1.0f,0.0f,0.0f ) );
+	XMVECTOR upVector;
+	if (pitch <= -PI / 2.0f || pitch >= PI / 2.0f)
+	{
+		upVector = XMVectorSet(0.0f, -1.0f, 0.0f, 0.0f);
+	}
+	else
+	{
+		upVector = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	}
+	return XMMatrixLookAtLH(camPosition,camTarget, upVector);
 }
 
 void Camera::SpawnControlWindow() noexcept
@@ -35,7 +44,7 @@ void Camera::SpawnControlWindow() noexcept
 		ImGui::SliderFloat( "Y",&pos.y,-80.0f,80.0f,"%.1f" );
 		ImGui::SliderFloat( "Z",&pos.z,-80.0f,80.0f,"%.1f" );
 		ImGui::Text( "Orientation" );
-		ImGui::SliderAngle( "Pitch",&pitch,0.995f * -90.0f,0.995f * 90.0f );
+		ImGui::SliderAngle( "Pitch",&pitch,-180.0f,180.0f );
 		ImGui::SliderAngle( "Yaw",&yaw,-180.0f,180.0f );
 		if( ImGui::Button( "Reset" ) )
 		{
@@ -54,41 +63,50 @@ void Camera::Reset() noexcept
 
 void Camera::LookZero() noexcept
 {
-	yaw = atan2(pos.x, pos.z) + PI;
-	pitch = atan2(pos.y, sqrt(pos.x*pos.x + pos.z* pos.z));
+	yaw = wrap_angle(atan2(pos.x, pos.z) + PI);
+	pitch = wrap_angle(atan2(pos.y, sqrt(pos.x*pos.x + pos.z* pos.z)));
 }
 
 void Camera::KeepLookFront(DirectX::XMFLOAT3 position) noexcept
 {
 	DirectX::XMFLOAT3 delta = { pos.x - position.x,pos.y - position.y, pos.z - position.z };
-	yaw = atan2(delta.x, delta.z) + PI;
-	pitch = atan2(delta.y, sqrt(delta.x*delta.x + delta.z* delta.z));
+	yaw = wrap_angle(atan2(delta.x, delta.z) + PI);
+	pitch = wrap_angle(atan2(delta.y, sqrt(delta.x*delta.x + delta.z* delta.z)));
 }
 
 void Camera::Rotate( float dx,float dy ) noexcept
 {
-
+	if (pitch <= -PI / 2.0f || pitch > PI / 2.0f)
+	{
+		dx = -dx;
+	}
 	yaw = wrap_angle( yaw + dx * rotationSpeed );
-	pitch = std::clamp( pitch + dy * rotationSpeed,0.995f * -PI / 2.0f,0.995f * PI / 2.0f );
+	pitch = wrap_angle(pitch + dy * rotationSpeed);
 }
 
 void Camera::RotateAround(float dx, float dy) noexcept
 {
 	using namespace dx;
 	// apply the camera rotations to a base vector
-	DirectX::XMFLOAT3 lookVector, destination = pos;
-	dx::XMStoreFloat3(&lookVector, XMVector3Transform(XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f), XMMatrixRotationRollPitchYaw(pitch, yaw, 0.0f)));
-	dx::XMStoreFloat3(&destination,
-		dx::XMVector3Transform(dx::XMLoadFloat3(&destination),
-			dx::XMMatrixTranslation(lookVector.x, lookVector.y, lookVector.z) *
-			dx::XMMatrixRotationRollPitchYaw(std::clamp(dy * rotationSpeed, 0.5f * -PI / 2.0f, 0.5f * PI / 2.0f),
-				wrap_angle(dx * rotationSpeed), 0.0f)));
-	dx::XMStoreFloat3(&pos,
-		dx::XMVector3Transform(dx::XMLoadFloat3(&pos),
-			dx::XMMatrixRotationRollPitchYaw(std::clamp(dy * rotationSpeed, 0.5f * -PI / 2.0f, 0.5f * PI / 2.0f),
-				wrap_angle(dx * rotationSpeed), 0.0f)));
-
-	KeepLookFront(destination);
+	//if (0.995f * -PI / 2.0f < pitch && pitch < 0.995f * PI / 2.0f)
+	{
+		DirectX::XMFLOAT3 lookVector, destination = pos;
+		dx::XMStoreFloat3(&lookVector, XMVector3Transform(XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f), XMMatrixRotationRollPitchYaw(pitch, yaw, 0.0f)));
+		dx::XMStoreFloat3(&destination,
+			dx::XMVector3Transform(dx::XMLoadFloat3(&destination),
+				dx::XMMatrixTranslation(lookVector.x, lookVector.y, lookVector.z) *
+				XMMatrixRotationQuaternion(XMQuaternionRotationAxis(XMVector3Transform(XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f),
+					XMMatrixRotationRollPitchYaw(0, atan2(pos.x, pos.z) + PI, 0.0f)), dy * rotationSpeed)) *
+				XMMatrixRotationQuaternion(XMQuaternionRotationAxis(XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f), dx * rotationSpeed))
+			));
+		XMStoreFloat3(&pos,
+			XMVector3Transform(XMLoadFloat3(&pos),
+				XMMatrixRotationQuaternion(XMQuaternionRotationAxis(XMVector3Transform(XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f),
+					XMMatrixRotationRollPitchYaw(0, atan2(pos.x, pos.z) + PI, 0.0f)), dy * rotationSpeed)) *
+				XMMatrixRotationQuaternion(XMQuaternionRotationAxis(XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f), dx * rotationSpeed))
+			));
+		KeepLookFront(destination);
+	}
 }
 
 void Camera::Translate( DirectX::XMFLOAT3 translation ) noexcept
--- hw3d/ChiliMath.h
+++ hw3d/ChiliMath.h
@@ -13,10 +13,19 @@ constexpr auto sq( const T& x )
 template<typename T>
 T wrap_angle( T theta )
 {
-	const T modded = fmod( theta,(T)2.0 * (T)PI_D );
-	return (modded > (T)PI_D) ?
+	T modded = fmod( theta,(T)2.0 * (T)PI_D );
+	if (modded > (T)PI_D)
+	{
+		modded -= (T)2.0 * (T)PI_D;
+	}
+	else if (modded < -(T)PI_D)
+	{
+		modded += (T)2.0 * (T)PI_D;
+	}
+	/*return (modded > (T)PI_D) ?
 		(modded - (T)2.0 * (T)PI_D) :
-		modded;
+		modded;*/
+	return modded;
 }
 
 template<typename T>
